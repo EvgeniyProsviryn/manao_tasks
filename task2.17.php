00d1bffbf1a53c7dc37a44c9e1bdf875670ffec0
Evgeniy <start_up_project@mail.ru>
2018-02-23 14:14:53 +0300

    task2.17

---
+++ task2.17.php
@@ -0,0 +1,29 @@
+<?php
+
+ini_set('display_errors', 'On');
+
+$n = 10;
+$mas = [];
+$count = 0;
+
+
+for($i = 0;$i<$n;$i++){
+    $mas[$i] = rand(10,20);
+    echo $mas[$i].' ';
+}
+
+echo '<hr>';
+
+$element = $mas[0];
+
+for($i = 0;$i<$n;$i++){
+    if($mas[$i] == $element){
+        $element = $mas[$i];
+        $count++;
+    }
+}
+
+echo "Количество элементов встречающихся более одного раза: $count";
+
+
+?>
